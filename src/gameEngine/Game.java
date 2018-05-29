ef439193a4215a2a936056df79450af89f107e71
guinardpaul <guinardpaul@gmail.com>
2018-05-29 09:28:08 +0200

    video 14 - animation and swimming

--- src/gameEngine/Game.java
+++ src/gameEngine/Game.java
@@ -19,7 +19,7 @@ import gameEngine.level.Level;
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 
-	public static int WIDTH = 160;
+	public static int WIDTH = 360;
 	public static int HEIGHT = WIDTH / 12 * 9;
 	public static int SCALE = 3;
 	public static String NAME = "Game";
@@ -73,7 +73,7 @@ public class Game extends Canvas implements Runnable {
 
 		screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/sprite_sheet.png"));
 		input = new InputHandler(this);
-		level = new Level("/levels/small_test_level.png");
+		level = new Level("/levels/water_test_level.png");
 		player = new Player(level, 0, 0, input);
 		level.addEntity(player);
 	}
--- src/gameEngine/level/tiles/BasicTile.java
+++ src/gameEngine/level/tiles/BasicTile.java
@@ -5,18 +5,23 @@ import gameEngine.level.Level;
 
 public class BasicTile extends Tile {
 
+	private final static int SHEET_WIDTH = 32;
 	protected int tileId;
 	protected int tileColour;
 
 	public BasicTile(int id, int x, int y, int tileColour, int levelColour) {
 		super(id, false, false, levelColour);
-		this.tileId = x + y;
+		this.tileId = x + y * SHEET_WIDTH;
 		this.tileColour = tileColour;
 	}
 
 	@Override
 	public void render(Screen screen, Level level, int x, int y) {
 		screen.render(x, y, tileId, tileColour, 0x00, 1);
+	}
+
+	@Override
+	public void tick() {
 	}
 
 }
